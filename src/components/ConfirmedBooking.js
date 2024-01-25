f5379725af64cbdd2349ace55c597ff63b3897aa
jaspeper <140134686+JasperThePark@users.noreply.github.com>
2024-01-25 12:54:30 +0800

    another committ

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ConfirmedBooking = () => {
+  return (
+    <div className="confirm">
+        <div>
+            <h1>Booking has been <span>confirmed!</span></h1>
+        </div>
+    </div>
+  )
+}
+
+export default ConfirmedBooking; 
---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className='menu-container'>
+        <div>
+            <h2>This weeks specials</h2>
+            <button>Order Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Menu
