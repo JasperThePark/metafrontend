d9bc989adc133c2f3c56a06d83bef478a3670129
jaspeper <140134686+JasperThePark@users.noreply.github.com>
2024-01-25 19:56:33 +0800

    probally last commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,51 @@
 import React from 'react'
+import recipes from "../recipes"
+import Swal from 'sweetalert2'
 
 const Menu = () => {
+
+    const handleOrder = () => {
+        console.log("pressed")
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, order it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Ordered!",
+                text: "Your order has been submitted.",
+                icon: "success"
+              });
+            }
+          });
+    }
   return (
     <div className='menu-container'>
-        <div>
+        <div className='menu-header'>
             <h2>This weeks specials</h2>
             <button>Order Menu</button>
+        </div>
+
+
+        <div className="cards">
+            {
+                recipes.map(recipe => <div key={recipe.id} className='menu-items'>
+                    <img src={recipe.image} alt = ""/>
+                    <div className='menu-content'>
+                        <div className='heading'>
+                            <h5>{recipe.title}</h5>
+                            <p>{recipe.price}</p>
+                        </div>
+                        <p>{recipe.description}</p>
+                        <button className="orderbtn" onClick={() => handleOrder(recipe.id)}>Order Now</button>
+                    </div>
+                </div>)
+            }
         </div>
     </div>
   )
